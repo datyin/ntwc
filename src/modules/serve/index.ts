450023889ef2930d2b19f8e5505ed615a03f48e1
Jove Jonovski <jovejonovski@gmail.com>
2021-06-29 10:03:32 +0200

    Config loader on serve

--- src/modules/serve/index.ts
+++ src/modules/serve/index.ts
@@ -1,5 +1,19 @@
+import log from '../../lib/logger';
 import globals from '../../global';
 
+import * as ntwc from '../../configs/ntwc';
+import * as pkg from '../../configs/package';
+import * as typescript from '../../configs/typescript';
+
 export default async (): Promise<void> => {
-  console.log(globals.argv);
+  console.clear();
+  log.info(`📚  Loading configuration files...`);
+
+  await ntwc.load();
+  await pkg.load();
+  await typescript.load();
+
+  log.print(`✔️  All configuration files were loaded.`);
+
+  console.log(globals.argv, pkg.config);
 };
