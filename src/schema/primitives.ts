2e1f9e05bf2ab95615d0cb4feb7511afe4cb9927
Jove Jonovski <jovejonovski@gmail.com>
2021-07-24 23:08:14 +0200

    JSON types

--- src/schema/primitives.ts
+++ src/schema/primitives.ts
@@ -1,1 +1,7 @@
 export type SemVerNumber = `${number}.${number}.${number}`;
+export type Primitive = bigint | boolean | null | number | string | symbol | undefined;
+export type JSONValue = Primitive | JSONObject | JSONArray;
+export type JSONArray = Array<JSONValue>;
+export interface JSONObject {
+  [key: string]: JSONValue;
+}
